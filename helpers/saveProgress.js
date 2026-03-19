de2594c67e4a9d1e207ebb4862624ab6fd51eb73
Sardor Farhod o'g'li <92437771+SardorDev12@users.noreply.github.com>
2026-03-19 11:01:23 +0500

    Fix quotation marks in saveProgress.js

--- helpers/saveProgress.js
+++ helpers/saveProgress.js
@@ -1,12 +1,12 @@
-const fs = require("fs");
-const { getTodayFile } = require("./getTodayFile");
+const fs = require('fs');
+const { getTodayFile } = require('./getTodayFile');
 
 const saveProgress = (input) => {
   const todayFile = getTodayFile();
   let quotes = [];
 
   if (fs.existsSync(todayFile)) {
-    const fileContent = fs.readFileSync(todayFile, "utf8");
+    const fileContent = fs.readFileSync(todayFile, 'utf8');
 
     if (fileContent.trim().length > 0) {
       quotes = JSON.parse(fileContent);
